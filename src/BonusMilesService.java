5b64c09af8348254991bf644ed6f9a25d34e2784
Neto test <test@neto.com>
2022-12-24 10:29:19 +0300

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price){
-        int cost = price/20;
-return cost;
+    public int calculate(int price) {
+        int cost = price / 20;
+        return cost;
 
     }
 
